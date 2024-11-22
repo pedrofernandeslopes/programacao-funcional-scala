72c62bcecbb8a049fd12b8cfc1781468159f52f8
pedro <pedroflopes1710@gmail.com>
2024-11-22 17:00:12 -0300

    Funções Recursivas - Ex04

---
+++ src/main/scala/funcoesRecursivas/Ex04.scala
@@ -0,0 +1,13 @@
+package funcoesRecursivas
+
+object Ex04 {
+    def fibonacci(n: Int): Int = {
+        if (n == 0) 0
+        else if (n == 1) 1
+        else fibonacci(n - 1) + fibonacci(n - 2)
+    }
+    
+    def main(args: Array[String]): Unit = {
+        println(fibonacci(8))
+    }
+}
